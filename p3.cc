9ed657be8e8396d50bff51fd49813b806df4c269
wonjegal <https://github.com/wonjegal/practice2.git>
2023-09-10 14:37:07 +0900

    add p3.cc

--- p3.cc
+++ p3.cc
@@ -1,3 +1,42 @@
 #include <iostream>
 
 using namespace std;
+
+// 생성자 : 맴버 변수초기화
+//
+class Complex{
+public:
+    Complex(){
+        real = 0;
+        imag = 0;
+    }
+    Complex(double real_, double imag_){
+        real = real_;
+        imag = imag_;
+    }
+    double GetReal(){
+        return real;
+    }
+    void SetReal(double real_){
+        real = real_;
+    }
+    double GetImag(){
+        return imag;
+    }
+    void SetImag(double imag_){
+        imag = imag_;
+    }
+private:
+    double real;
+    double imag;
+};
+
+int main(void){
+    Complex c1;
+    Complex c2(2,3);
+
+    cout << "C1 = " << c1.GetReal() << " , " << c1.GetImag() << endl;
+    cout << "C2 = " << c2.GetReal() << " , " << c2.GetImag() << endl;
+}
+
+
